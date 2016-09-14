1a28acea7f120b95a184acfe4f65aac65b18a536
faooful <faooful@gmail.com>
2016-09-14 15:11:39 -0700

    pushing script to inject images from a directory to the web page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+var dir = "../images/";
+var fileextension = ".gif";
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+        //List all gifs file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+            $("body").append($("<img src=" + dir + filename + "></img>"));
+        });
+    }
+});
+
+//Filter the images
+const input = document.querySelector('input')
+
+input.addEventListener('input', e => {
+  document.querySelectorAll('img').forEach(img => {
+    const src = img.getAttribute('src')
+    const value = e.target.value
+
+    if (value.length > 0 && src.indexOf(value) !== -1) {
+      img.classList.remove('hidden')
+    } else {
+      img.classList.add('hidden')
+    }
+
+    if (value.length === 0) {
+      img.classList.remove('hidden')
+    }
+  })
+})
